9512b819326aacfe994acd22525fc706cbc4867d
Shriyash <ghimires1@my.smccd.edu>
2026-06-12 15:25:39 -0700

    feat: allow Anonymous messages

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -3,7 +3,7 @@ import messages from "../models/messages_db.js";
 const addNewMessage = (request, response) => {
   messages.push({
     text: request.body.textMessage,
-    user: request.body.userName,
+    user: request.body.userName || "Anonymous",
     added: new Date(),
   });
 
