f48b2cfef990eb77bc4d8158127b2e6005304a6d
Shriyash <ghimires1@my.smccd.edu>
2026-06-18 14:00:52 -0700

    chore : import correct module to access env variables

--- models/db/pool.js
+++ models/db/pool.js
@@ -1,5 +1,7 @@
 import { Pool } from "pg";
 import { argv } from "node:process";
+import { env } from "node:process";
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
